fd43704b2ed54359d1b1903127924a2a5c0e8f00
Kevin Bernard <kevin.bernard@zoho.com>
2021-10-31 14:09:59 +0100

    [func-fix] Fin CRUD user et fix des erreurs (downgrade Mongoose 5.10.13, ajout UseFindAndModify: false in follow and unfollow functions, and useNewUrlParser: true, useUnifiedTopology: true to db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose
     .connect(
-        "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.ydsna.mongodb.net/mern-project")
+        "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.ydsna.mongodb.net/mern-project",{ useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB", err));
