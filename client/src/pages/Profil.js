c91a12a4210ce2f667413014e756bf8531a3bc6a
Kevin Bernard <kevin.bernard@zoho.com>
2021-11-01 18:10:30 +0100

    [func] Developed the sign in front end form, created user context with cookie to either display the signin form or the profile index on the profile URL

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Log from '../components/Log';
+import { UidContext } from '../components/AppContext';
 
 const Profil = () => {
+
+    const uid = useContext(UidContext);
+
     return (
-        <div>
-            Voici le profil
+        <div className="profil-page">
+            {uid ? (
+                <h1>UPDATE PAGE</h1>
+            ) : (
+                <div className="log-container">
+                    <Log signin={false} signup={true} />
+                    <div className="img-container">
+                        <img src="./img/log.svg" alt="Login" />
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
