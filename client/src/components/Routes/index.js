3e944d0d5d33ba04c946bb0a266c2bed6e0d6763
Kevin Bernard <kevin.bernard@zoho.com>
2021-11-02 20:59:31 +0100

    [func] Navbar with login/logout redirection and dynamic display

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 import Home from '../../pages/Home';
 import Profil from '../../pages/Profil';
 import Trending from '../../pages/Trending';
+import Navbar from '../Navbar';
 
 const index = () => {
     return (
             <Router>
+                <Navbar />
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/profil" exact component={Profil} />
